15e152b492c1cf284a8353a0f648d35b4a2ba8d2
tamutamu <tamu.0.0.tamu@gmail.com>
2017-08-07 16:41:44 +0900

    Modify debug code.

--- nablarch-build.sh
+++ nablarch-build.sh
@@ -14,6 +14,9 @@ cd ${TRAVIS_BUILD_DIR}
 # if it merges pull request to develop branch or dilectly commit on develop branch, execute `gradlew uploadArchives`.
 # Waning, TRAVIS_PULL_REQUEST variable is 'false' or pull request number, 1,2,3 and so on.
 if [ "${TRAVIS_PULL_REQUEST}" == "false" -a "${TRAVIS_BRANCH}" == "develop"  ]; then
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ aaaa @@@@@@@@@@@@@@@@@@@@@@@"
+  hostname
+
   ./gradlew clean test uploadArchives -PnablarchRepoUsername=${NABLARCH_USER} -PnablarchRepoPassword=aaa \
                            -PnablarchRepoReferenceUrl=${DEVELOP_REPO_URL} -PnablarchRepoReferenceName=${DEVELOP_REPO_NAME} \
                            -PnablarchRepoDeployUrl=dav:${DEVELOP_REPO_URL} -PnablarchRepoName=${DEVELOP_REPO_NAME} \
