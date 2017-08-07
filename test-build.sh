1bc71ef0f54897838d8d5a414173fcceff4cd38e
tamutamu <tamu.0.0.tamu@gmail.com>
2017-08-07 15:34:37 +0900

    Modify debug code.

--- test-build.sh
+++ test-build.sh
@@ -12,8 +12,8 @@ export DEVELOP_REPO_NAME=repo
 # if it creates pull request, execute `gradlew build` only.
 # if it merges pull request to develop branch or dilectly commit on develop branch, execute `gradlew uploadArchives`.
 # Waning, TRAVIS_PULL_REQUEST variable is 'false' or pull request number, 1,2,3 and so on.
-##if [ "${TRAVIS_PULL_REQUEST}" == "false" -a "${TRAVIS_BRANCH}" == "develop"  ]; then
-if false; then
+if [ "${TRAVIS_PULL_REQUEST}" == "false" -a "${TRAVIS_BRANCH}" == "develop"  ]; then
+##if false; then
   ./gradlew clean test uploadArchives -PnablarchRepoUsername=${NABLARCH_USER} -PnablarchRepoPassword=${NABLARCH_PASS} \
                            -PnablarchRepoReferenceUrl=${DEVELOP_REPO_URL} -PnablarchRepoReferenceName=${DEVELOP_REPO_NAME} \
                            -PnablarchRepoDeployUrl=dav:${DEVELOP_REPO_URL} -PnablarchRepoName=${DEVELOP_REPO_NAME} \
