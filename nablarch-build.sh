36dae53374a0c0a4392995f32fb3eb5cc8b8804b
tamutamu <tamu.0.0.tamu@gmail.com>
2017-08-07 17:12:02 +0900

    Modify debug code.

--- nablarch-build.sh
+++ nablarch-build.sh
@@ -8,23 +8,7 @@ source $HOME/build-script/travis-ci/nablarch_env
 
 cd ${TRAVIS_BUILD_DIR}
 
-
-### Main Build.
-# if it creates pull request, execute `gradlew build` only.
-# if it merges pull request to develop branch or dilectly commit on develop branch, execute `gradlew uploadArchives`.
-# Waning, TRAVIS_PULL_REQUEST variable is 'false' or pull request number, 1,2,3 and so on.
-if [ "${TRAVIS_PULL_REQUEST}" == "false" -a "${TRAVIS_BRANCH}" == "develop"  ]; then
-  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ aaaa @@@@@@@@@@@@@@@@@@@@@@@"
-  hostname
-
-  ./gradlew clean test -PnablarchRepoUsername=${NABLARCH_USER} -PnablarchRepoPassword=aaa \
+./gradlew clean test -PnablarchRepoUsername=${NABLARCH_USER} -PnablarchRepoPassword=aaa \
                            -PnablarchRepoReferenceUrl=${DEVELOP_REPO_URL} -PnablarchRepoReferenceName=${DEVELOP_REPO_NAME} \
                            -PnablarchRepoDeployUrl=dav:${DEVELOP_REPO_URL} -PnablarchRepoName=${DEVELOP_REPO_NAME} \
                            -PdevelopLibUrl=${DEVELOP_REPO_URL}/${DEVELOP_REPO_NAME}
-else
-  ./gradlew clean test -PnablarchRepoUsername=${NABLARCH_USER} -PnablarchRepoPassword=aaa \
-                  -PnablarchRepoReferenceUrl=${DEVELOP_REPO_URL} -PnablarchRepoReferenceName=${DEVELOP_REPO_NAME} \
-                  -PnablarchRepoDeployUrl=dav:${DEVELOP_REPO_URL} -PnablarchRepoName=${DEVELOP_REPO_NAME} \
-                  -PdevelopLibUrl=${DEVELOP_REPO_URL}/${DEVELOP_REPO_NAME}
-fi
-
